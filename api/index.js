1bc26e4bc7992ecb8d442b7423d92581bd013219
Tomket Lovers <tomketloversofc@gmail.com>
2024-11-03 08:43:41 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,36 +1,24 @@
-const express = require('express');
-const cors = require('cors');
 const axios = require('axios');
 
-const app = express();
-const PORT = 3000;
+export default async function handler(req, res) {
+    if (req.method === 'GET') {
+        try {
+            const response = await axios.get('https://www.gate.io/apiw/v2/pre_market/currencies/TOMA');
+            
+            const tomaData = response.data.data;
+            const latestDealPrice = parseFloat(tomaData.latest_deal_price);
 
-// Middleware CORS
-app.use(cors());
-
-// Endpoint untuk mengambil harga TOMA dari Gate.io
-app.get('/api/', async (req, res) => {
-    try {
-        // Fetch data dari API Gate.io
-        const response = await axios.get('https://www.gate.io/apiw/v2/pre_market/currencies/TOMA');
-        
-        // Ambil data yang dibutuhkan
-        const tomaData = response.data.data;
-        const latestDealPrice = parseFloat(tomaData.latest_deal_price);
-
-        // Kirim data sebagai respons
-        res.json({
-            currency: tomaData.currency,
-            full_name: tomaData.full_name,
-            latest_deal_price: latestDealPrice
-        });
-    } catch (error) {
-        console.error('Error fetching data from Gate.io:', error);
-        res.status(500).json({ error: 'Error fetching data' });
+            res.status(200).json({
+                currency: tomaData.currency,
+                full_name: tomaData.full_name,
+                latest_deal_price: latestDealPrice
+            });
+        } catch (error) {
+            console.error('Error fetching data from Gate.io:', error);
+            res.status(500).json({ error: 'Error fetching data' });
+        }
+    } else {
+        res.setHeader('Allow', ['GET']);
+        res.status(405).end(`Method ${req.method} Not Allowed`);
     }
-});
-
-// Jalankan server
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
+}
